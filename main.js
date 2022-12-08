f4afa844d2bbaa7a92f4855731f5e39b133c31ba
Kuza <leonardo04renteria@gmail.com>
2022-12-07 22:22:58 -0700

    Tercer modificacion, se implemento el menu de ordenes

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
 const burguerMenu = document.querySelector('.menu')
+const menuCartIcon = document.querySelector('.navbar-shopping-cart')
+const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const orderMenu = document.querySelector('.product-detail')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 burguerMenu.addEventListener('click', toggleMobileMenu)
+menuCartIcon.addEventListener('click', toggleOrderMenu)
 
 
 function toggleDesktopMenu() {
+    orderMenu.classList.add('inactive')
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+    orderMenu.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleOrderMenu() {
+    desktopMenu.classList.add('inactive')
+    mobileMenu.classList.add('inactive')
+    orderMenu.classList.toggle('inactive')
 }
